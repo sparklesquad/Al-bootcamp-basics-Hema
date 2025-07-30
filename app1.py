c5382798de948ca46ae091b5da6d758f8d651a6c
hematejaswi@78gmail.com <sparklesquad>
2025-07-30 13:45:15 +0530

    uploading app1.py

---
+++ app1.py
@@ -0,0 +1,18 @@
+name="mlewc"
+num=10
+isOn=True
+letter="H"
+#opeartors-1
+#Arithemetic- +-*/%
+#Logical- and(&) or(|) not(~)
+#comparision -<,>,==,!=,<=,>=
+#assignment - =,+=,-=,*=,/=
+#control statements-2
+if(num>5):
+    print(name)
+else:
+    print(isOn)    
+#loops
+# while loop - while(num>5):
+    
+          
